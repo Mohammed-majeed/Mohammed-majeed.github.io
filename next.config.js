fa0e5ed0fdcf18f85f64e26f6fc274b4c523c7d0
mohammed-majeed <mohammed.y.majeed@gmail.com>
2024-07-10 23:28:39 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   trailingSlash: true, // Ensure that all paths are rendered with trailing slashes
   exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
     return {
@@ -15,5 +11,12 @@ module.exports = {
   },
   images: {
     unoptimized: true, // Disable image optimization for static exports
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
+
+module.exports = nextConfig;
