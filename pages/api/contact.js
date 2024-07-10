15b17733952c61e822ed4ef390c72a695acfe559
mohammed-majeed <mohammed.y.majeed@gmail.com>
2024-07-10 23:54:16 +0200

    update

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,11 +1,11 @@
-export default function handler(req, res) {
-    if (req.method === 'POST') {
-        const { name, email, message } = req.body;
+// export default function handler(req, res) {
+//     if (req.method === 'POST') {
+//         const { name, email, message } = req.body;
 
-        // Perform your backend logic here, like sending an email or saving to a database
+//         // Perform your backend logic here, like sending an email or saving to a database
 
-        res.status(200).json({ message: 'Message sent successfully' });
-    } else {
-        res.status(405).json({ message: 'Method not allowed' });
-    }
-}
+//         res.status(200).json({ message: 'Message sent successfully' });
+//     } else {
+//         res.status(405).json({ message: 'Method not allowed' });
+//     }
+// }
